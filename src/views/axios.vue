f50e8ff67203019c89b04ef5974fe4c885dc0df4
codytang <codytang@tencent.com>
2021-05-21 15:57:49 +0800

    feat: add axios

--- src/views/axios.vue
+++ src/views/axios.vue
@@ -1,3 +1,17 @@
 <template>
   <div>Axios</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import axios from '../utils/axios';
+
+export default defineComponent({
+  setup() {
+    axios
+      .get('/user/XPoet')
+      .then((res) => console.log('res', res))
+      .catch((err) => console.error('err', err));
+  },
+});
+</script>
